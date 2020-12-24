6372d80c819ead5e1becc6d79807c7c24b90bba3
chathuravithakshana <chathuravithakshana@gmail.com>
2020-12-25 02:59:54 +0530

    AdMod Added - not working

--- app/src/main/java/com/darkilluminate/compass_findyourdirection/MainActivity.java
+++ app/src/main/java/com/darkilluminate/compass_findyourdirection/MainActivity.java
@@ -2,13 +2,110 @@ package com.darkilluminate.compass_findyourdirection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    //device sensor manager
+    private SensorManager sensorManager;
+
+    //define the compass image
+    private ImageView imgCompass;
+
+    //record the angle turned of the compass picture
+    private float degreeStart = 0f;
+
+    TextView appHeading;
+
+    AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgCompass = (ImageView) findViewById(R.id.img_compass);
+        appHeading = (TextView) findViewById(R.id.txt_heading);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        adView = findViewById(R.id.ad_view_bottom);
+
+        /*MobileAds.initialize(this,"ca-app-pub-1418713931658974~5010600597");
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);*/
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //to stop the listener and save battery
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //code for system's orientation sensor registered listeners
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+
+        appHeading.setText("Direction: " + Float.toString(degree) + " degrees from North");
+
+        // rotation animation - reverse turn degree degrees
+        RotateAnimation ra = new RotateAnimation(
+                degreeStart,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        // set the compass animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // set how long the animation for the compass image will take place
+        ra.setDuration(210);
+
+        // Start animation of compass image
+        imgCompass.startAnimation(ra);
+        degreeStart = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
